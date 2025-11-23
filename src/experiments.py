83f3f0f91c42453b63b2dc887f336ad4b93bd364
Jiachen Liu <jl315@illinois.edu>
2025-11-23 16:30:08 -0600

    Modified experiments

--- src/experiments.py
+++ src/experiments.py
@@ -1,30 +1,59 @@
 # experiments.py
 
+import argparse
+import Example
 import time, os, psutil
 import numpy as np
 from dataloader import load_ner_dataset, load_cls_dataset, load_sts_dataset
 import spacy
+from utils import measure_runtime, get_memory_usage, get_model_size, save_results
 
-# 1. Load models (Basic version & Transformer version)
-def load_models():
-    nlp_base = spacy.load("en_core_web_md")      # Standard spaCy
-    nlp_trf = spacy.load("en_core_web_trf")      # spaCy-transformers
-    return nlp_base, nlp_trf
+# Load models
+def load_models(model_name):
+    nlp_model = spacy.load(model_name) 
+    return nlp_model
 
-# Three experiments: NER / Classification / Similarity
-def run_similarity_experiment(nlp_base, nlp_trf, sts_dataset):
+# NER Experiment
+def run_ner_experiment(nlp_model, ner_dataset):
+    # measuring runtime for NER recognition
+    results, runtime = measure_runtime(lambda: [nlp_model(d["text"]) for d in ner_dataset])
+    
+    # convert examples for spaCy evaluation
+    spacy_examples = [
+        Example.from_dict(nlp_model.make_doc(ex["text"]), {"entities": ex["entities"]})
+        for ex in ner_dataset
+    ]
+
+    scores = nlp_model.evaluate(spacy_examples)
+
+    return {
+        "precision": scores["ents_p"],
+        "recall": scores["ents_r"],
+        "f1": scores["ents_f"],
+        "runtime_sec": runtime,
+        "memory_mb": get_memory_usage(),
+        "model_size_mb": get_model_size(nlp_model.path)
+    }
+    
+    
+# Classification Experiment
+def run_classification_experiment(nlp_model, cls_dataset):
     ...
-def run_classification_experiment(nlp_base, nlp_trf, cls_dataset):
-    ...
-def run_ner_experiment(nlp_base, nlp_trf, ner_dataset):
+    
+# NER Experiment
+def run_similarity_experiment(nlp_model, sts_dataset):
     ...
 
 if __name__ == "__main__":
-    nlp_base, nlp_trf = load_models()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--model", type=str, required=True, help="Name of the model to evaluate")
+    args = argparser.parse_args()
+    
+    nlp_model = load_models(args.model)
     ner_ds = load_ner_dataset()
     cls_ds = load_cls_dataset()
     sts_ds = load_sts_dataset()
 
-    run_similarity_experiment(nlp_base, nlp_trf, sts_ds)
-    run_classification_experiment(nlp_base, nlp_trf, cls_ds)
-    run_ner_experiment(nlp_base, nlp_trf, ner_ds)
+    run_similarity_experiment(nlp_model, sts_ds)
+    run_classification_experiment(nlp_model, cls_ds)
+    run_ner_experiment(nlp_model, ner_ds)
